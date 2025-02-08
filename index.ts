4aa4fdc86e9f5d030ae5cfb7e82c378536ba09e4
Noemi <69471841+noemiowo@users.noreply.github.com>
2025-02-08 03:33:06 +0100

    German (#3)
    * partly translation into german
    
    ignore this repo if you see this, i'm just making a translation for [Unbound](https://github.com/unbound-mod)
    
    * continuation with de-DE.json
    
    god my hands hurt from typing so much on my phone
    
    * finished de-DE.json
    
    * add de-DE to index.ts
    
    ---------
    
    Co-authored-by: Mario P. <me@marioparaschiv.com>

--- index.ts
+++ index.ts
@@ -2,10 +2,11 @@
 import esES from '@i18n/es-ES.json';
 import enUS from '@i18n/en-US.json';
 import enGB from '@i18n/en-GB.json';
-
+import deDE from '@i18n/de-DE.json';
 
 export default {
 	'en-US': enUS,
 	'en-GB': enGB,
+	'de-DE': deDE,
 	'es-ES': esES,
 };
